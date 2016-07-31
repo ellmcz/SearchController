c1b09405ea792ccfefffc7504762936087b00233
Ellmcz <wqbs007@163.com>
2016-07-31 21:19:07 +0800

    修改bug

--- SearchController/SearchHeader.h
+++ SearchController/SearchHeader.h
@@ -23,42 +23,5 @@
 #define Property_hAllPigYin(Name)  @property (nonatomic, copy) NSString *propertyAllPinYin##Name;
 #define Property_hSubAllPigYin(Name)  @property (nonatomic, copy) NSString *propertySubAllPinYin##Name;
 //
-#define tableViewData_h()\
-@property (nonatomic, strong) NSMutableArray *pinYinData;\
-@property (nonatomic, strong) NSMutableArray *totalData;\
-@property (nonatomic, strong) NSMutableArray *data;
-//
-#define tableViewData_m()\
--(NSMutableArray *)data{\
-if (!_data ) {\
-_data = [[NSMutableArray alloc] init];\
-}\
-return _data;\
-}\
--(NSMutableArray *)pinYinData{\
-if (!_pinYinData ) {\
-_pinYinData = [[NSMutableArray alloc] init];\
-}\
-return _pinYinData;\
-}\
-- (NSMutableArray *)totalData{\
-if (!_totalData ) {\
-_totalData = [[NSMutableArray alloc] init];\
-}\
-return _totalData;\
-}\
-
-#define tableViewDataSource_m()\
--(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{\
-return self.totalData.count;\
-}\
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{\
-return [self.totalData[section] count];\
-}
-//
-#define tableViewSectinTitle_m()\
--(NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView{\
-    return [self.pinYinData copy];\
-}
 
 #endif /* SearchHeader_h */
