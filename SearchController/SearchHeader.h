35bf168d330b67d4f2f88f524fb41c010a5e6c57
Ellmcz <wqbs007@163.com>
2016-08-01 11:52:15 +0800

    修改了头文件

--- SearchController/SearchHeader.h
+++ SearchController/SearchHeader.h
@@ -8,14 +8,69 @@
 
 #ifndef SearchHeader_h
 #define SearchHeader_h
-
+#if __has_include(<SearchHeader/SearchHeader.h>)
+FOUNDATION_EXPORT double SortPropertyVersion;
+FOUNDATION_EXPORT const unsigned char SortPropertyVersionString[];
+#import <SearchHeader/NSArray+Sort.h>
+#import <SearchHeader/NSString+Chinese.h>
+#import <SearchHeader/NSMutableArray+Only.h>
+#import <SearchHeader/NSString+Pure.h>
+#import <SearchHeader/NSString+Property.h>
+#import <SearchHeader/SortedProperty.h>
+#import <SearchHeader/NSMutableArray+Sort.h>
+#else
 #import "NSArray+Sort.h"
 #import "NSString+Chinese.h"
 #import "SortedProperty.h"
 #import "NSMutableArray+Only.h"
 #import "NSMutableArray+Sort.h"
 #import "NSString+Pure.h"
+#import "NSString+Property.h"
+#endif
+
+#if __has_include(<SearchHeader/SearchHeader.h>)
+#import <SearchHeader/SearchHeader.h>
+#elif __has_include(<SearchHeader/SearchHeader.h>)
+#import <SearchHeader/NSArray+Sort.h>
+#import <SearchHeader/NSString+Chinese.h>
+#import <SearchHeader/NSMutableArray+Only.h>
+#import <SearchHeader/NSString+Pure.h>
+#import <SearchHeader/NSString+Property.h>
+#import <SearchHeader/SortedProperty.h>
+#import <SearchHeader/NSMutableArray+Sort.h>
+#import <SearchHeader/SearchController.h>
+#else
+#import "NSArray+Sort.h"
+#import "NSString+Chinese.h"
+#import "SortedProperty.h"
+#import "NSMutableArray+Only.h"
+#import "NSMutableArray+Sort.h"
+#import "NSString+Pure.h"
+#import "NSString+Property.h"
 #import "SearchController.h"
+#endif
+
+#if __has_include(<SearchHeader/SearchHeader.h>)
+#import <SearchHeader/SearchHeader.h>
+#elif __has_include(<SearchHeader/SearchHeader.h>)
+#import <SearchHeader/NSArray+Sort.h>
+#import <SearchHeader/NSString+Chinese.h>
+#import <SearchHeader/NSMutableArray+Only.h>
+#import <SearchHeader/NSString+Pure.h>
+#import <SearchHeader/NSString+Property.h>
+#import <SearchHeader/SortedProperty.h>
+#import <SearchHeader/NSMutableArray+Sort.h>
+#import <SearchHeader/SearchController.h>
+#else
+#import "NSArray+Sort.h"
+#import "NSString+Chinese.h"
+#import "SortedProperty.h"
+#import "NSMutableArray+Only.h"
+#import "NSMutableArray+Sort.h"
+#import "NSString+Pure.h"
+#import "NSString+Property.h"
+#import "SearchController.h"
+#endif
 
 //
 #define Property_hSubFirstPinYin(Name)  @property (nonatomic, copy) NSString *propertyFirstPinYin##Name;
